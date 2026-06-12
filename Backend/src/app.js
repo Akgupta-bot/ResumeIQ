85c52fb2d4778dc3b027485551251b6eb909d828
Anurag kumar gupta <anurag1243456@gmail.com>
2026-06-12 20:56:02 +0530

    Fix CORS origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,12 +7,20 @@ const app =express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://resume-iq-silk.vercel.app/login"
-    ],
+    origin: function(origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "https://resume-iq-silk.vercel.app"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
-}))
+}));
 const authRouter=require("./routes/auth.routes")
 app.use("/api/auth",authRouter)
 app.use("/api/interview", interviewRouter)
